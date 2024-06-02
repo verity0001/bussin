b944709a51a71b072f513b95ba70bd39c6ec420c
verity <80880870+verity0001@users.noreply.github.com>
2024-06-02 14:39:30 +0000

    s

--- script.lua
+++ script.lua
@@ -1,5 +1,7 @@
-local input = get("input")
+-- local input = get("input")
+local h1 = get("h1")
+h1.setcontent("sus");
 
-input.on_input(function(content)
-    print(content)
-end)
+-- input.on_input(function(content)
+--     h1.setcontent("sus");
+-- end)
